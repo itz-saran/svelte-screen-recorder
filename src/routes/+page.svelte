f269c6d1a1594f7aac89a5fc2e796cfc13fca954
saran <saran.v@codingmart.com>
2023-11-29 18:23:02 +0530

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
 	import Recorder from "$components/recorder.svelte";
 </script>
 
+<svelte:head>
+	<title>Screen Recorder</title>
+	<meta
+		name="description"
+		content="A screen recoder made using screen capture and media recorder APIs"
+	/>
+</svelte:head>
+
 <Recorder preferCurrentTab={false} />
